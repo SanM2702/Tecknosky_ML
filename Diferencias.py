d4ad40d4a6e1f9a93138c88f15775cdc358afd12
Juan Luguito <juanmet1234@gmail.com>
2025-08-18 20:58:31 -0500

    Agrego primer archivo de las diferencias entre IA y ML

---
+++ Diferencias.py
@@ -0,0 +1,15 @@
+'''
+DIFERENCIAS ENTRE IA Y ML -------------------------------V1---------------------------------------
+
+La inteligencia artificial (IA) es un término que engloba diferentes estrategias y técnicas que
+se usan para hacer que las máquinas se parezcan más a los humanos. La IA incluye todo, desde asistentes
+inteligentes como Alexa hasta aspiradoras robóticas y vehículos autónomos. El machine learning (ML) 
+es una de las muchas otras ramas de la IA. El machine learning es la ciencia del desarrollo
+de algoritmos y modelos estadísticos que los sistemas informáticos utilizan para realizar
+tareas complejas sin instrucciones explícitas. En cambio, los sistemas se basan en patrones e 
+inferencias. Los sistemas de computación utilizan algoritmos de machine learning para procesar 
+grandes cantidades de datos históricos e identificar patrones de datos. Si bien el machine learning 
+es IA, no todas las actividades de IA son machine learning.
+
+'''
+
