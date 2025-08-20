75fa9481326986bfba1af3c3f6c85d8368945a0d
Oscar David Jaime Bedoya <ojaimebedoya@gmail.com>
2025-08-20 10:47:21 -0500

    Conclusion de la informacion

---
+++ CONCLUSION.py
@@ -0,0 +1,15 @@
+'''CONCLUSION:
+La inteligencia artificial y el machine learning representan un avance 
+tecnológico que impacta directamente en la vida cotidiana y en sectores clave como 
+la medicina, la industria, el comercio y la seguridad.
+
+Mientras la IA persigue el objetivo global de que las máquinas 
+realicen tareas humanas complejas de manera autónoma y eficiente, el ML se centra en 
+el análisis de datos y el aprendizaje automático a partir de ellos.
+
+Ambas tecnologías son complementarias: la IA establece el propósito general
+de la inteligencia en las máquinas, y el ML provee los métodos prácticos para 
+alcanzarlo. En conjunto, permiten desarrollar sistemas cada vez más precisos, autónomos
+e innovadores, capaces de transformar la forma en que interactuamos con la tecnología y
+resolvemos problemas en el mundo real.
+'''
