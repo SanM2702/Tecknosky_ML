c17cce026960d241a3e2ce0f30c243802b210aeb
JuanCamero <juanchito200621@gmail.com>
2025-08-20 08:48:10 -0500

    Primer tipo de ML
    Agrego el tipo numero uno ML, mencionando nombre y definición del mismo

---
+++ Tipos.py
@@ -0,0 +1,11 @@
+'''
+TIPOS DE MACHINE LEARNING:
+
+1).Aprendizaje supervisado
+
+Consiste en entrenar modelos usando conjuntos de datos con etiquetas conocidas (input–output). Es útil para predicciones y clasificación.
+Algoritmos comunes: regresión lineal, redes neuronales, árboles de decisión, SVM.
+Usos: detección de fraudes, reconocimiento de imágenes, predicción de ventas, clasificación de documentos, entre otros.
+
+
+'''
