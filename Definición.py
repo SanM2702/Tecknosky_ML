3cd219665cbee35eb907f542918d9f15a0dedb61
Santiago Martinez <marsan2700@gmail.com>
2025-08-20 07:11:07 -0500

    Definición de IA
    Creo el primer archivo en relación a la definición de Inteligencia Artifical

---
+++ Definición.py
@@ -0,0 +1,27 @@
+'''
+DEFINICIÓN DE IA Y ML
+
+Inteligencia Artifical:
+La inteligencia artificial (IA) es una rama de la informática dedicada a la creación de sistemas que pueden replicar y simular 
+la inteligencia humana, lo que permite a las máquinas llevar a cabo tareas que anteriormente solo podían hacer las personas. 
+Estas actividades abarcan el aprendizaje basado en la experiencia, el entendimiento del lenguaje natural, la identificación de 
+imágenes, el análisis de grandes cantidades de datos, el razonamiento, la resolución de problemas, la toma de decisiones, la 
+formulación de recomendaciones y, en algunos casos, la exhibición de creatividad y autonomía. 
+
+Para alcanzar estos objetivos, la IA se fundamenta en metodologías avanzadas como el aprendizaje automático (Machine learning) 
+y el aprendizaje profundo (Deep Learning), que facilitan que los sistemas se ajusten y se perfeccionen continuamente a partir 
+de la información disponible. El aprendizaje automático utiliza algoritmos que detectan patrones en los datos y crean modelos 
+predictivos, mientras que el aprendizaje profundo se vale de redes neuronales artificiales, inspiradas en la estructura del 
+cerebro humano, para procesar la información de manera más elaborada y exacta. 
+
+La inteligencia artificial no solo pretende que las máquinas "razonen" como los seres humanos, sino que también aspire a 
+sobrepasar las limitaciones humanas en cuanto a velocidad, precisión y capacidad de análisis, permitiendo el procesamiento 
+de vastas cantidades de información en cuestión de segundos. Por ejemplo, una IA es capaz de examinar millones de registros 
+médicos para encontrar patrones que no son perceptibles para un humano, traducir idiomas al instante, sugerir productos en 
+plataformas en línea o gestionar automóviles sin conductor. 
+
+En conclusion, la inteligencia artificial es la disciplina que transforma los sistemas computacionales en entidades inteligentes, 
+capaces de aprender, adaptarse y tomar decisiones, con aplicaciones que van desde la vida diaria hasta áreas clave como la 
+medicina, la industria, el comercio, la seguridad y la investigación científica. 
+
+'''
